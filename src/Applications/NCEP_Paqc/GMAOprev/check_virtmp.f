820204851ae699fcd9eda3d9003635712769adfe
Meta Sienkiewicz <Meta.Sienkiewicz@nasa.gov>
2021-11-16 15:04:30 -0500

    Adding routine to check if SFCSHP in input prepbufr files already has VIRTMP correction applied, and if so, to skip applying it in the GMAOprev step.

---
+++ src/Applications/NCEP_Paqc/GMAOprev/check_virtmp.f
@@ -0,0 +1,52 @@
+        implicit none
+        real*8 hdr(1), tpc(1,20)
+        real*8 bmiss, getbmiss
+        CHARACTER(len=180) filn
+        CHARACTER(len=8)  SUBSET
+        integer  iret, idate, ilev, j, tvflg, lubfi
+        real vtcd
+
+        data lubfi /10/
+        
+        call getarg(1,filn)
+        open(unit=lubfi,file=filn,form='unformatted')
+
+        CALL OPENBF(LUBFI,'IN',LUBFI)
+        call ufbqcd(lubfi,'VIRTMP',vtcd)
+        tvflg = 1
+        subset = ''
+        iret = 0
+        do while (iret == 0 .and. subset /= 'SFCSHP')
+           CALL READMG(LUBFI,SUBSET,IDATE,IRET)
+        end do
+
+
+C  LOOP THROUGH THE INPUT MESSAGES - READ THE NEXT SUBSET
+C  ------------------------------------------------------
+
+        do while(iret .eq. 0) 
+          CALL READSB(LUBFI,IRET)
+          IF(IRET .eq. 0) then
+            call ufbint(lubfi,hdr,1,1,ilev,'TYP')
+            call ufbevn(lubfi,tpc,1,1,20,ilev,'TPC')
+            do j = 1,20
+               if(tpc(1,j) == vtcd) tvflg = 0
+               if(tpc(1,j) >= bmiss) exit
+            end do
+            if (tvflg == 0) exit
+          else
+              subset = ''
+              iret = 0
+              do while (iret == 0 .and. subset /= 'SFCSHP')
+                 CALL READMG(LUBFI,SUBSET,IDATE,IRET)
+              end do
+          endif
+        end do
+        if (tvflg == 0) then
+           print *,'VIRTUAL'
+        else
+           print *,'DRY'
+        end if
+
+        stop
+        end
