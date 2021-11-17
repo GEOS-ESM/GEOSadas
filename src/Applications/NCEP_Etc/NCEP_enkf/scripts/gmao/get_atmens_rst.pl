4985b6b83c291b2dff87a55c82dff0af72dcdc04
Meta Sienkiewicz <Meta.Sienkiewicz@nasa.gov>
2021-11-17 10:42:27 -0500

    replace some files that were modified in merge of 'develop'

--- src/Applications/NCEP_Etc/NCEP_enkf/scripts/gmao/get_atmens_rst.pl
+++ src/Applications/NCEP_Etc/NCEP_enkf/scripts/gmao/get_atmens_rst.pl
@@ -1,12 +1,10 @@
 #!/usr/bin/env perl
 use strict;
 use warnings;
-use Cwd qw(abs_path cwd);
+use Cwd qw(cwd);
 use File::Basename qw(basename dirname);
 use File::Copy qw(cp mv);
 use File::Path qw(mkpath rmtree);
-use Getopt::Long qw(GetOptions);
-
 use FindBin qw($Bin);
 use lib "$Bin";
 use Manipulate_time qw(tick);
@@ -32,7 +30,7 @@ my %vopts = ( "verbose" => 1 );
     my ($atmens_stat_dir, $atmens_ebkg_dir, $atmens_erst_dir, $atmens_ecbkg_dir);
     my ($tarfile, $tarpath, $label, $pid);
     my ($ens, $mem, $mfile, $mfile_new);
-    my (@archList, @tarList);
+    my (@tarList);
     
     init();
     chdir($atmens_dir);
@@ -54,12 +52,12 @@ my %vopts = ( "verbose" => 1 );
     foreach $label ("stat", "ebkg", "ecbkg", "erst") {
         $tarfile = "$expid.atmens_$label.${yyyymmdd}_${hh}z.tar";
         $tarpath = "$atmens_date_dir/$tarfile";
-        push @archList, $tarpath if archFile($tarpath);
         push @tarList, $tarpath;
     }
+
     defined($pid = fork) or die "Error while attempting to fork;";
     unless ($pid) {
-        system "dmget @archList";
+        system "dmget @tarList";
         exit;
     }
     foreach $tarpath (@tarList) { system_("tar xvf $tarpath") }
@@ -90,6 +88,7 @@ my %vopts = ( "verbose" => 1 );
 # purpose - get runtime parameters and flags
 #=======================================================================
 sub init {
+    use Getopt::Long qw(GetOptions);
     my ($fvhome, $help, $exparcdir);
 
     GetOptions("fvhome=s" => \$fvhome,
@@ -103,7 +102,6 @@ sub init {
     ($exparcdir, $newid, $yyyymmdd, $hh) = @ARGV;
 
     $exparcdir =~ s/[\s\/]*$//;
-    $exparcdir = abs_path($exparcdir);
     $arcdir = dirname($exparcdir);
     $expid = basename($exparcdir);
 
@@ -123,20 +121,6 @@ sub init {
     $atmens_dir = "$fvhome/atmens";
     mkpath($atmens_dir, \%vopts) unless -d $atmens_dir;
     
-}
-
-#=======================================================================
-# name - archFile
-# purpose - Return true (1) if $file is an archive file;
-#           Return false (0) if not
-#=======================================================================
-sub archFile {
-    my ($file, $dmLine, $dmFLG);
-    $file = shift @_;
-    $dmLine = `dmls -l $file`;
-    $dmFLG = 1;
-    $dmFLG = 0 if $dmLine =~ m|(N/A)|;
-    return $dmFLG;
 }
 
 #=======================================================================
