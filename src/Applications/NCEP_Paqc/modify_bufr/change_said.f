4b8ecfdb4876aff9e86a5d8e229ba26e277ad925
gmao-msienkie <gmao-msienkie@users.noreply.github.com>
2024-04-10 15:26:26 -0400

    Added code to modify the maximum bufr output size

--- src/Applications/NCEP_Paqc/modify_bufr/change_said.f
+++ src/Applications/NCEP_Paqc/modify_bufr/change_said.f
@@ -30,17 +30,18 @@
 !-----------------------------------------------------------------------
 
       integer luin, luout       !  unit numbers
-      
+
       integer argc
       integer(4) iargc,iarg
       integer      ireadmg
       integer      ireadsb
-      
+
       character*120 inputfile
       character*120 outputfile
       character*8 newid, oldid
+      character*10 cmaxout
       character*1 cf
-      
+
       character*8  subset       ! name of current BUFR subset
 
       integer      idate        ! synoptic date/time YYYYMMDD
@@ -48,14 +49,15 @@
       integer      iret         ! subroutine return code
 
       integer      klev, llev   ! no. of levels in report
+      integer      length, imaxout
 
-      
- 
+
+
       integer  m,n              ! report counter
 
       real       roldid         ! old sat ID to change
       real       rnewid         ! new sat ID to use
-      
+
       real(8) hdr
 
       data luin /8/, luout /9/
@@ -70,6 +72,18 @@
       cf = 'N'
       iarg = 0
 
+      CALL GET_ENVIRONMENT_VARIABLE('BUFR_MAXOUT',cmaxout,length)
+      if (length > 0) then
+        read(cmaxout,*) imaxout
+        if (imaxout > 15000) then
+          call maxout(imaxout)
+        else
+          call maxout(15000)
+        end if
+      else
+      call maxout(15000)
+      end if
+
       call GetArg( 1_4, oldID)
       if (argc .gt. 4 .and. oldID .eq. '-c') then
         iarg = 1
@@ -79,51 +93,51 @@
       call GetArg( 2_4+iarg, newID)
       call GetArg( 3_4+iarg, inputfile)
       call GetArg( 4_4+iarg, outputfile)
-      
+
       open(unit=luin,file=trim(inputfile),form='unformatted')
       open(unit=luout,file=trim(outputfile),form='unformatted')
       call openbf(luin,'IN ',luin)
       call openbf(luout,'OUT',luin)
 
       call cmpmsg(cf)
-      
+
       m = 0
       n = 0
       read(oldID,*) roldid
       read(newID,*) rnewid
       print *,'Will change old satID=',roldid,' to newid=',rnewid
-      
+
       do while (ireadmg(luin,subset,idate).eq. 0)
-         
+
          call openmb(luout,subset,idate)
-         
+
          do while ( ireadsb(luin) .eq. 0 )
-            
-!     
+
+!
 !     Copy individual reports from input message buffer to
 !     output message buffer
-!     
+!
             call ufbcpy(luin, luout)
-            
+
 ! read in satID and write out modified satID
-               
+
             call ufbint(luin,hdr,1,1,klev,'SAID')
             if (klev .eq. 1 .and. hdr .eq. roldid) then
                hdr = rnewid
                call ufbint(luout,hdr,1,1,llev,'SAID')
                m = m + 1
             end if
-            
+
             n = n + 1
-!     
+!
 !  write output buffer to output file
-!     
+!
             call writsb(luout)
          enddo
-         
+
          call closmg(luout)
-         
-      enddo      
+
+      enddo
       call closbf(luout)
 
       print *,'modified ',m,' of ',n,' records'
